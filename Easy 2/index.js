2ff77edd37e87661ac56ba93b38fda98e4e3c16f
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2025-12-04 09:35:05 +0530

    done 2

--- Easy 2/index.js
+++ Easy 2/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
+const timestamp = require("./middlewares/middleware");
 const app = express();
 
-
-app.get("/protected", (req, res) => {
+app.use(timestamp)
+app.get("/protected", timestamp, (req, res) => {
   // TODO: Send the response: Access granted at <timestamp>
-  
+  const timestamp = req.timestamp
+  console.log(timestamp)
 });
-
-
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
